3102804ffcbdb0f56e9a7418370b9df0eb9078dd
AFakhriddinov <112176053+AFakhriddinov@users.noreply.github.com>
2023-05-04 13:36:00 +0500

    Fix linter errors

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -8,10 +8,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
+          <code>src/App.js</code> and save to reload.
         </p>
+        <p>Hello world</p>
         <a
           className="App-link"
           href="https://reactjs.org"
