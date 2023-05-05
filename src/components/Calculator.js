5467fb7cbe409c15843116a544ec7fb5f75ff3a7
AFakhriddinov <112176053+AFakhriddinov@users.noreply.github.com>
2023-05-05 08:44:53 +0500

    Add events to calculator app

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,44 +1,100 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
 import './Calculator.css';
+import calculate from './logic/calculate';
 
-const Calculator = () => (
-  <>
+const Calculator = () => {
+  const [state, updateState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const calculation = (e) => {
+    const newState = calculate(state, e.target.textContent);
+    updateState(newState);
+  };
+
+  return (
     <div className="main">
-      <Operand />
+      <Operand data={state} />
       <div className="buttons">
         <div className="left-section">
-          <div className="ac">AC</div>
-          <div className="plusOrMinus">+/-</div>
-          <div className="modelus">%</div>
-          <div className="seven">7</div>
-          <div className="eight">8</div>
-          <div className="nine">9</div>
-          <div className="four">4</div>
-          <div className="five">5</div>
-          <div className="six">6</div>
-          <div className="1">1</div>
-          <div className="two">2</div>
-          <div className="three">3</div>
-          <div className="zero">0</div>
-          <div className="dot">.</div>
+          <button type="button" onClick={calculation} className="ac">
+            AC
+          </button>
+          <button type="button" onClick={calculation} className="plusOrMinus">
+            +/-
+          </button>
+          <button type="button" onClick={calculation} className="modelus">
+            %
+          </button>
+          <button type="button" onClick={calculation} className="seven">
+            7
+          </button>
+          <button type="button" onClick={calculation} className="eight">
+            8
+          </button>
+          <button type="button" onClick={calculation} className="nine">
+            9
+          </button>
+          <button type="button" onClick={calculation} className="four">
+            4
+          </button>
+          <button type="button" onClick={calculation} className="five">
+            5
+          </button>
+          <button type="button" onClick={calculation} className="six">
+            6
+          </button>
+          <button type="button" onClick={calculation} className="1">
+            1
+          </button>
+          <button type="button" onClick={calculation} className="two">
+            2
+          </button>
+          <button type="button" onClick={calculation} className="three">
+            3
+          </button>
+          <button type="button" onClick={calculation} className="zero">
+            0
+          </button>
+          <button type="button" onClick={calculation} className="dot">
+            .
+          </button>
         </div>
         <div className="right-section">
-          <div className="plus">/</div>
-          <div className="plus">x</div>
-          <div className="plus">-</div>
-          <div className="plus">+</div>
-          <div className="plus">=</div>
+          <button type="button" onClick={calculation} className="plus">
+            /
+          </button>
+          <button type="button" onClick={calculation} className="plus">
+            x
+          </button>
+          <button type="button" onClick={calculation} className="plus">
+            -
+          </button>
+          <button type="button" onClick={calculation} className="plus">
+            +
+          </button>
+          <button type="button" onClick={calculation} className="plus">
+            =
+          </button>
         </div>
       </div>
     </div>
-  </>
-);
+  );
 
-const Operand = () => (
-  <>
-    <div className="operand-input">
-      <input type="text" name="operand" value="0" />
-    </div>
-  </>
-);
+  function Operand(props) {
+    const state = props;
+    return (
+      <div className="operand-input">
+        <input
+          type="text"
+          name="operand"
+          value={state.data.next || state.data.total || 0}
+        />
+      </div>
+    );
+  }
+};
 export default Calculator;
