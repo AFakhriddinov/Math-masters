85ad5cd2843ce0144bdd490fc213c061af51dbbc
AFakhriddinov <112176053+AFakhriddinov@users.noreply.github.com>
2023-05-18 12:11:34 +0500

    Add test for calculate.js file

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,22 +4,31 @@ import '../App.css';
 import calculate from './logic/calculate';
 
 const Calculator = () => {
-  const [state, updateState] = useState({
-    total: null,
-    operation: null,
-    next: null,
-  });
+  // const [state, updateState] = useState({
+  //   total: null,
+  //   operation: null,
+  //   next: null,
+  // });
 
+  // const calculation = (e) => {
+  //   const newState = calculate(state, e.target.textContent);
+  //   updateState(newState);
+  // };
+  const [state, SetState] = useState({});
   const calculation = (e) => {
-    const newState = calculate(state, e.target.textContent);
-    updateState(newState);
+    SetState(calculate(state, e.target.textContent));
   };
+  const { next, total, operation } = state;
+  const display = (total || '') + (operation || '') + (next || '') || '0';
 
   return (
     <div className="main-section">
       <h2>Lets do some Math</h2>
       <div className="main">
-        <Operand data={state} />
+        {/* <Operand data={state} /> */}
+        <div className="operand-input" data-testid="display">
+          {display}
+        </div>
         <div className="buttons">
           <div className="left-section">
             <button type="button" onClick={calculation} className="ac">
@@ -87,17 +96,18 @@ const Calculator = () => {
     </div>
   );
 
-  function Operand(props) {
-    const state = props;
-    return (
-      <div className="operand-input">
-        <input
-          type="text"
-          name="operand"
-          value={state.data.total || state.data.operation || state.data.next}
-        />
-      </div>
-    );
-  }
+  // function Operand(props) {
+  //   const state = props;
+  //   return (
+  //     <div className="operand-input">
+  //       <input
+  //         data-testid="display"
+  //         type="text"
+  //         name="operand"
+  //         value={state.data.total || state.data.operation || state.data.next}
+  //       />
+  //     </div>
+  //   );
+  // }
 };
 export default Calculator;
