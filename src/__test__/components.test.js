1c5749036c4eeaa0eb4620d245ceda60c2eb7260
AFakhriddinov <112176053+AFakhriddinov@users.noreply.github.com>
2023-05-18 14:51:21 +0500

    Add snapshot for Quote.js file

--- src/__test__/components.test.js
+++ src/__test__/components.test.js
@@ -3,11 +3,14 @@ import { render } from "@testing-library/react";
 import Home from "../components/Home";
 import Quote from "../components/Quote";
 
-describe("Testing Home, Quote, and Navigation components render correctly", () => {
+describe("Testing Home, and Quote components render correctly", () => {
   it("Rendering Home component", () => {
     const { asFragment } = render(<Home />);
     expect(asFragment()).toMatchSnapshot();
   });
 
-  
+  it("Rendering Quote component", () => {
+    const { asFragment } = render(<Quote />);
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
